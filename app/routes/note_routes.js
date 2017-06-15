646ee6a9f98533d2dcc266cc2c5dd78ac9ce0246
Benjamin <benj@zagorski.com>
2017-06-15 14:32:44 -0700

    General POST and GET are working locally

---
+++ app/routes/note_routes.js
@@ -0,0 +1,41 @@
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
+  // app.get('/not', function(req, res) {
+  //   res.send(req)
+  // }),
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+  app.get('/notes', (req, res) => {
+    // const id = req.params.id;
+    // const details = { '_id': new ObjectID(id) };
+    db.collection('notes').find().toArray((err, items) => {
+      console.log(items);
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(items);
+      }
+    });
+  });
+
+  app.post('/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection("notes").insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+};
